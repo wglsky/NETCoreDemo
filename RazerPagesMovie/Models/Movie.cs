205f98cb7cc6f9661a8976fb788315cdf18cc1dc
Guoliang Wang <guoliang.wang@nov.com>
2022-08-08 10:23:14 +0200

    Added model - Movie

---
+++ RazerPagesMovie/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazerPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        
+        [DataType(DataType.DateTime)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+
+    }
+}
